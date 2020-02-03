24681593c6bf4953ab33669626d41f1d0865fa93
Ray.Cheng <840644236@qq.com>
2020-02-03 18:32:42 +0800

    测试提及交

---
+++ src/test/java/com/qf/tests/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.qf.tests;
+
+/**
+ * @Author Ray.Cheng
+ * @Date 2020/2/3
+ */
+public class GitHubTest {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
